0ac2a2fbec7e0c38456085c9122afebde4fde935
hexiao <2608019886@qq.com>
2023-01-17 15:59:20 +0800

    文本弹窗、用户信息

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pyautogui
 import tkinter as tk
+import typing as t
+
+
+class UserData:
+    user_name: t.Optional[str] = None
+    user_type: t.Optional[t.Literal[0, 1, 2]] = None
+    user_identity: t.Optional[int] = None
+    user_info: str = ''
 
 
 class WindowConfigParams:
@@ -55,6 +63,11 @@ class MainWindow(WindowConfigParams):
         tk.Text(self.root, bg=self.background_color).place(x=0, y=self.title_bar_height, width=self.root_width,
                                                            height=self.root_height - self.title_bar_height)
 
+        self.a = UserData()
+        info_button = tk.Button(self.root, text='发言内容', bg=self.title_background_color, activebackground='white',
+                                font=("bold", 14), fg='black', command=pop_with_user(self.a))
+        info_button.place(x=100, y=200)
+
     def reset_info(self):
         """初始化数据"""
         self.mode_val.set('会议模式')
@@ -66,6 +79,27 @@ class MainWindow(WindowConfigParams):
             self.mode_val.set('游戏模式')
         else:
             self.mode_val.set('会议模式')
+
+
+def pop_with_user(current_user: UserData):
+    def pop_win_command():
+        pop_win = tk.Toplevel()
+        pop_win.title("发言信息")
+        pop_win.attributes('-toolwindow', True)
+        pop_win.geometry("300x200+200+200")
+        text_input = tk.Text(pop_win)
+        text_input.pack(fill="both", expand=True)
+        text_input.focus_set()
+        text_input.insert(tk.END, current_user.user_info)
+
+        def close_win(ev=None):
+            current_user.user_info = text_input.get('0.0', tk.END)
+            pop_win.destroy()
+
+        pop_win.bind("<Escape>", close_win)
+        pop_win.protocol('WM_DELETE_WINDOW', close_win)
+
+    return pop_win_command
 
 
 main_loop = MainWindow()
